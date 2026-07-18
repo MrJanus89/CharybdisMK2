3d2acf0cb465e264331c2ec2306e891219bfb7ae
MrJanus89 <sks890224@gmail.com>
2026-07-19 00:34:17 +0900

    Update input_processor_threshold_layer.c

--- custom_modules/threshold_module/src/input_processor_threshold_layer.c
+++ custom_modules/threshold_module/src/input_processor_threshold_layer.c
@@ -9,7 +9,7 @@
 
 #define DT_DRV_COMPAT zmk_input_processor_threshold_layer
 
-#define THRESHOLD_MOVEMENT_UNITS 32U
+#define THRESHOLD_MOVEMENT_UNITS 256U
 
 struct threshold_layer_data {
     int32_t accumulated;
